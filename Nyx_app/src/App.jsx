9b114fa889dd8b97d543fb1c3f0f9dc60c6d2391
Misaki <misaki.irema@gmail.com>
2026-09-03 18:22:49 +0200

    not polished output of chat

--- Nyx_app/src/App.jsx
+++ Nyx_app/src/App.jsx
@@ -2,8 +2,17 @@ import { useState } from "react";
 //import "./App.css";
 
 function App() {
-  const [input, setText] = useState("");
-  const [messages, setMessage] = useState("");
+  const [input, setInput] = useState("");
+  const [history, setHistory] = useState("");
+
+  const getHistory = async () => {
+    const req = await fetch("http://localhost:3000/chat/history", {
+      method: "GET",
+      headers: { "Content-Type": "application/JSON" },
+    });
+    const data = await req.json();
+    setHistory(data);
+  };
 
   const handleInput = async () => {
     if (input.trim() !== "") {
@@ -14,19 +23,24 @@ function App() {
       });
       const data = await req.json();
       console.log(data.reply);
-      setText("");
+      //do the whole thing with messages. show the history e.t.c. first function to store it. then the showcase on page fix extraction from the memory of the chat
+
+      setInput("");
     }
   };
 
   return (
     <>
       <div>
+        {history.reply &&
+          history.reply.map((msg, index) => <p key={index}>{msg.content}</p>)}
         <input
           value={input}
-          onChange={(e) => setText(e.target.value)}
+          onChange={(e) => setInput(e.target.value)}
           onKeyDown={(e) => {
             if (e.key === "Enter") {
               handleInput();
+              getHistory();
             }
           }}
         />
